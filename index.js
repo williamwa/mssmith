1cd7572288fc44f8ca79081eca3c4f5820c59716
William <arenabpo@gmail.com>
2023-03-23 21:18:27 +0800

    add config

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 import express from "express";
 import { Telegraf } from "telegraf";
 
-const bot = new Telegraf(token);
+require('dotenv').config();
+
+const bot = new Telegraf(process.env.BOT_TOKEN);
 const app = express();
 
 // Set the bot API endpoint
-app.use(await bot.createWebhook({ domain: webhookDomain }));
+app.use(await bot.createWebhook({ domain: process.env.DOMAIN }));
 
 bot.on("text", ctx => ctx.reply("Hello"));
 
-app.listen(port, () => console.log("Listening on port", port));
+app.listen(port, () => console.log("Listening on port", process.env.PORT));
