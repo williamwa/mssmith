b1978a6e26088004dffdd7bb703da9e87ed92fd1
William <arenabpo@gmail.com>
2023-03-24 20:46:58 +0800

    remember last 10 sentence

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import { Telegraf } from "telegraf";
 import * as dotenv from 'dotenv';
+import * as cache from 'memory-cache';
 
 dotenv.config();
 
@@ -28,15 +29,27 @@ bot.on("text", async ctx => {
     const question = ctx.message.text;
 
     console.log('message', ctx.message);
+    const chat_id = ctx.message.chat.id;
+
+    const cached_messages = cache.get(chat_id);
+    let messages = cached_messages ? cached_messages : [];
+    messages.push({ role: 'user', content: question });
 
     const completion = await openai.createChatCompletion({
         model: "gpt-3.5-turbo",
-        messages: [
-            { role: 'user', content: question }
-        ],
+        messages: messages,
     });
-    const answer = completion.data.choices[0].message.content
+    const answer = completion.data.choices[0].message.content;
+
+    messages = messages.push({ role: 'system', content: answer });
     //console.log(question, answer);
+
+    if(messages.length > 10){
+        messages = messages.slice(-10);
+    }
+    console.log('messages', messages);
+
+    cache.put(chat_id, messages, 10*60*1000);
 
     ctx.reply(answer)
 });
